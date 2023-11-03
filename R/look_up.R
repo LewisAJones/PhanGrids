823bac1bcf7fbbfb438c38b7a2605aea25bd3aae
Lewis A. Jones <41071747+LewisAJones@users.noreply.github.com>
2023-11-03 13:36:50 +0100

    clean up repo

--- R/look_up.R
+++ R/look_up.R
@@ -1,50 +1,48 @@
 # Header ----------------------------------------------------------------
 # Project: GPM-reconstruction-grids
 # File name: look_up.R
-# Last updated: 2023-06-19
+# Last updated: 2023-11-03
 # Author: Lewis A. Jones
 # Email: LewisA.Jones@outlook.com
 # Repository: https://github.com/LewisAJones/GPM-reconstruction-grids
 # -----------------------------------------------------------------------
 
 # This script provides an example of how to generate palaeocoordinates for
-# fossil occurrences using the reconstruction files.
+# fossil collections using the reconstruction files.
 
 # Load libraries --------------------------------------------------------
 #install.packages("h3jsr")
 library(h3jsr)
-#install.packages("palaeoverse")
-library(palaeoverse)
 
 # Load table ------------------------------------------------------------
 # Let's use the Merdith et al. (2021) reconstruction file at h3 resolution 
 # 2 for an example
-df <-readRDS(file = "./R/resolution_2.RDS")
+df <-readRDS(file = "./data/resolution_2.RDS")
 
 # Load fossil data ------------------------------------------------------
-# Let's load example fossil occurrence data from the palaeoverse R package
-data("tetrapods")
+# Let's load example fossil collection data from the Paleobiology Database
+pbdb <- read.csv(file = "./data/pbdb_data.csv")
 
-# Assign h3 address to fossil occurrence data ---------------------------
-tetrapods$h3 <- point_to_cell(input = tetrapods[ , c("lng", "lat")], res = 2)
+# Assign h3 address to fossil collection data ---------------------------
+pbdb$h3 <- point_to_cell(input = pbdb[ , c("lng", "lat")], res = 2)
 
 # Match palaeocoordinates -----------------------------------------------
-# We need an age estimate for our fossil occurrence
+# We need an age estimate for our fossil collections
 # Let's use the age midpoint for simplicity
-tetrapods$age <- (tetrapods$min_ma + tetrapods$max_ma) / 2
+pbdb$age <- (pbdb$min_ma + pbdb$max_ma) / 2
 # And it should be rounded to the nearest whole number
-tetrapods$age <- round(tetrapods$age)
+pbdb$age <- round(pbdb$age)
 # Now let's match!
 # First we get the appropriate rows
-row_match <- match(x = tetrapods$h3, table = df$h3)
+row_match <- match(x = pbdb$h3, table = df$h3)
 # Next, the appropriate columns
-lng_match <- match(x = paste0("lng_", tetrapods$age), table = colnames(df))
-lat_match <- match(x = paste0("lat_", tetrapods$age), table = colnames(df))
+lng_match <- match(x = paste0("lng_", pbdb$age), table = colnames(df))
+lat_match <- match(x = paste0("lat_", pbdb$age), table = colnames(df))
 # Get the coordinates
 p_lng <- sapply(1:length(row_match), function(x) df[row_match[x], lng_match[x]])
 p_lat <- sapply(1:length(row_match), function(x) df[row_match[x], lat_match[x]])
 # Bind data
-coords <- cbind.data.frame(p_lng, p_lat)
+coords <- cbind(p_lng, p_lat)
 # Add the coordinates!
-tetrapods <- cbind.data.frame(tetrapods, coords)
+pbdb <- cbind.data.frame(pbdb, coords)
 
--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# GPM-reconstruction-grids
+# Earth surface evolution: a Phanerozoic gridded dataset of Global Plate Model reconstructions 
+
+Author(s): [Lewis A. Jones](mailto:LewisA.Jones@outlook.com) and [Mathew Domeier](mailto:mathewd@uio.no)
+
+This repository contains the data and code required to run the analyses and results of the article, "Earth surface evolution: a Phanerozoic gridded dataset of Global Plate Model reconstructions " (Jones and Domeier, 2023). 
+
+To cite the paper: 
+
+> Jones, L.A. and Domeier, M.M. 2023. Earth surface evolution: a Phanerozoic gridded dataset of Global Plate Model reconstructions. (TBC).
+
+To cite this repository:
+
+> Jones, L.A. and Domeier, M.M. 2023. Earth surface evolution: a Phanerozoic gridded dataset of Global Plate Model reconstructions. GitHub Repository: https://github.com/LewisAJones/GPM-reconstruction-grids
+
+-------
+
+## Data
+All data used in preparation of this article are included in this repository.
+
+* `data/` contains `pbdb_data.csv`, a dataset of fossil collections from [the Palaeobiology Database](https://paleobiodb.org/#/).
+* `data/` contains `resolution_2.RDS`, an example reconstruction file for H3 resolution 2 using the Merdith et al. (2021) Global Plate Model.
+
+NOTE: All reconstructions files are deposited on a dedicated [Zenodo repository]().
+
+-------
+
+## Python
+
+* `python/` contains the subfolder `plate_models`, which contains the static polygons and rotation files for the five Global Plate Models used here:
+
+| Abbreviation | Temporal coverage | Reference                |
+| ------------ | ----------------- | ------------------------ |
+| WR13         | 0–550 Ma          | (Wright et al., 2013)    |
+| MA16         | 0–410 Ma          | (Matthews et al., 2016)  |
+| TC16         | 0–540 Ma          | (Torsvik and Cocks, 2016)|
+| SC16         | 0–1100 Ma         | (Scotese, 2016)          |
+| ME21         | 0–1000 Ma         | (Merdith et al., 2021)   |
+
+* `python/` contains `make_grids.ipynb`, which provides a Jupyter Notebook documenting the process used to make the reconstruction grids.
+* `python/` contains `look_up.ipynb`, which provides a Jupyter Notebook which can be used to generate reconstructed coordinates for user data from the reconstruction files.
+
+-------
+
+## R
+
+* `R/` contains the subfolder `figures`, which provides the R code used to generate figures for the article.
+* `R/` contains `look_up.R`, which provides an R script which can be used to generate reconstructed coordinates for user data from the reconstruction files.
+
+
+
+ 
